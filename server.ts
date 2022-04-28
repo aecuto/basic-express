ad788cd2674f479722660c8404bc579d73b480bb
aecuto <kampee.expertly01@gmail.com>
2022-04-28 09:59:50 +0700

    change to typescript

--- server.ts
+++ server.ts
@@ -1,43 +1,47 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 
-const app = express();
+const app: Express = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/", async (req: Request, res: Response) => {
+  res.send({ msg: "Hello World!" });
+});
+
 // headers and query
 // localhost:3333/headers-query?test=123
-app.get("/headers-query", async (req, res) => {
+app.get("/headers-query", async (req: Request, res: Response) => {
   res.send({ query: req.query, headers: req.headers });
 });
 
 // post
 // localhost:3333/movie
-app.post("/movie", async (req, res) => {
+app.post("/movie", async (req: Request, res: Response) => {
   res.send({ body: req.body });
 });
 
 // get
 // localhost:3333/movies
-app.get("/movies", async (req, res) => {
+app.get("/movies", async (req: Request, res: Response) => {
   res.send({ msg: "movies list" });
 });
 
 // get by id
 // localhost:3333/movie/ss123
-app.get("/movie/:movieId", async (req, res) => {
+app.get("/movie/:movieId", async (req: Request, res: Response) => {
   res.send({ msg: "detail movie", params: req.params });
 });
 
 // edit by id
 // localhost:3333/movie/ss123
-app.put("/movie/:movieId", async (req, res) => {
+app.put("/movie/:movieId", async (req: Request, res: Response) => {
   res.send({ msg: "edit movie", params: req.params });
 });
 
 // delete by id
 // localhost:3333/movie/ss123
-app.delete("/movie/:movieId", async (req, res) => {
+app.delete("/movie/:movieId", async (req: Request, res: Response) => {
   res.send({ msg: "delete movie", params: req.params });
 });
 
